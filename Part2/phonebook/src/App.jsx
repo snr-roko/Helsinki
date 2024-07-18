a16e93844b1908881d1594580f956b6b0d0e4571
mr-r <rbbagyei@gmail.com>
2024-07-18 13:55:31 +0000

    Exercise 2.9 - A search field implemented: The app component maps 'personToShow' when rendering. This variable returns person when there is nothing in the input search field but filters the contacts when there is input. The search input is case insensitive and the variable in question was implemented using a ternery operator.

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '0506905193'
-     }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchString, setSearchString] = useState('')
+
+  const handleSearchString = (event) => {
+    setSearchString(event.target.value)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -32,9 +33,19 @@ const App = () => {
     }
   }
 
+  const personToshow = !searchString 
+    ? persons 
+    : persons.filter(person => {
+      const length = searchString.length
+      const search = searchString.toLowerCase()
+      const target = person.name.slice(0, length).toLowerCase()
+      return target === search
+    })
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter Name with <input value={searchString} onChange={handleSearchString} /></div>
       <form onSubmit={handleSubmit} >
         <div>
           <div>name: <input value={newName} onChange={handleNameChange} /></div>
@@ -46,7 +57,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => 
+        {personToshow.map(person => 
         <p key={person.name}>{person.name} {person.number}</p>
         )}
       </div>
